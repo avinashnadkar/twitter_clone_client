4dd1df56d0f214e98b51087a00de41c198c8c681
avinashnadkar <avinashnadkar3@gmail.com>
2021-10-01 22:52:09 +0530

    Created signup form with basic form validations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SignupForm from './Components/SignupForm/SignupForm';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      <SignupForm/>
     </div>
   );
 }
