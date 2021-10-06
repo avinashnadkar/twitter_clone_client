4e4dfecb533811cf8746d5e2567730cfaa529aa5
avinashnadkar <avinashnadkar3@gmail.com>
2021-10-06 10:17:00 +0530

    Protected routed (implemented user authentication on fronend)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,25 @@
+import {useContext} from "react";
 import Feed from "../../Components/Feed/Feed";
 import Navbar from "../../Components/Navbar/Navbar";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import style from "./Home.module.css"
+import { AuthContext } from "../../Context/AuthContextProvider";
+import { Redirect } from "react-router-dom";
 
 const Home = () => {
-    return(
+
+    const {isAuth} = useContext(AuthContext );
+
+    return (isAuth)?(
         <div className={style.homePage}>
             <Navbar/>
             <Feed/>
             <Sidebar/>
         </div>
+    ): (
+         <Redirect to={"/signup"}  />
     )
+
 }
 
 export default Home;
