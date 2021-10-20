7a70c7c79153fc9fcbbe51b2d5e518d85183a2e2
avinashnadkar <avinashnadkar3@gmail.com>
2021-10-20 22:17:40 +0530

    created explore page and created routes for this page and provided link in navbar

---
+++ src/Components/ExploreFeed/ExploreFeed.jsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import TweetCard from "../TweetCard/TweetCard";
+import SearchIcon from '@mui/icons-material/Search';
+import style from "./ExploreFeed.module.css";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../Context/AuthContextProvider";
+
+const ExploreFeed = () => {
+
+    ///////Context Functions and data //////////
+    const { webToken, user_id,userData } = useContext(AuthContext);
+
+    //States 
+    const [tweets,setTweets] = useState([]);
+
+    //Get tweets 
+    useEffect(() => {
+        const headers = {
+            'Content-Type': 'application/json',
+            "x-auth-token" : webToken
+        }
+
+        axios.get("http://localhost:2345/tweets/tweets",
+            {
+                headers: headers
+            }
+        )
+        .then(res => {
+            //set State of Tweets
+            setTweets(res.data)
+            //console.log(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [])
+
+    return (
+        // wrap code in react fragment
+        <>
+            <div className={style.feed}>
+                <div className={style.feedHeader}>
+                    <div className={style.top}>
+                        <div className={style.serachBox}>
+                            <button> <SearchIcon className={style.searchIcon}/> </button>
+                            <input type="text" placeholder="Search Twitter"/>
+                        </div>
+                        <div>
+                            set
+                        </div>
+                    </div>
+                    <div>
+                         <ul className={style.tabLinks}>
+                             <li><p>For you</p></li>
+                             <li><p>Covid-19</p></li>
+                             <li><p>Trending</p></li>
+                             <li><p>News</p></li>
+                             <li><p>Sports</p></li>
+                             <li><p>Entertainment</p></li>
+                         </ul>
+                    </div>
+                </div>
+      
+                <div className={style.tweetsContainer}>
+                    {
+                        tweets.map(el=>{
+                        return <TweetCard name={el.name} username="username" tweet={el.tweet} />
+                        })
+                    }  
+                </div>      
+            </div>
+        </>
+    )
+}
+
+export default ExploreFeed;
