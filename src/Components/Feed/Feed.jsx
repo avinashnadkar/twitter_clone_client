b75fcddd2dbf72bab3325a9c0ba8f837ca95503e
avinashnadkar <avinashnadkar3@gmail.com>
2021-11-29 23:01:02 +0530

    Created like feature (created a network request)

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -13,14 +13,15 @@ const Feed = () => {
 
     //States 
     const [tweets,setTweets] = useState([]);
+    
+    //header for http requests
+    const headers = {
+        'Content-Type': 'application/json',
+        "x-auth-token" : webToken
+    }
 
     //Get tweets 
     useEffect(() => {
-        const headers = {
-            'Content-Type': 'application/json',
-            "x-auth-token" : webToken
-        }
-
         axios.get("http://localhost:2345/tweets/tweets",
             {
                 headers: headers
@@ -35,6 +36,19 @@ const Feed = () => {
         })
     }, [])
 
+    //Like tweet
+    const handleLikeTweet = (tweetID,u_ID) => {
+        axios.post('http://localhost:2345/tweets/like',
+            {
+                "tweet_id" : tweetID,
+                "u_id" : u_ID
+            },
+            {
+                headers : headers
+            }
+        )
+    }
+
     return (
         // wrap code in react fragment
         <>
@@ -43,7 +57,18 @@ const Feed = () => {
                 <TweetInput />
                 {
                     tweets.map(el=>{
-                      return <TweetCard profilePic="https://pbs.twimg.com/profile_images/1236731619110719489/JyHmGkgb_400x400.jpg" name={el.name} username={el.username} tweet={el.tweet} media={el.media} likesCount={5} retweetCount={14} replyCount={22} showActionBar={true} tweetID={el._id}/>
+                      return <TweetCard profilePic="https://pbs.twimg.com/profile_images/1236731619110719489/JyHmGkgb_400x400.jpg" 
+                               name={el.name} 
+                               username={el.username} 
+                               tweet={el.tweet} 
+                               media={el.media} 
+                               likesCount={5} 
+                               retweetCount={14} 
+                               replyCount={22} 
+                               showActionBar={true} 
+                               tweetID={el._id} 
+                               likeTweet={()=>{handleLikeTweet(el._id,el.u_id)}}
+                             />
                     })
                 }           
             </div>
